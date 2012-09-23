4452633bb014a959325e4b2bd45e89e1770943bb
Igor Cândido <igr.cndd@gmail.com>
2012-09-23 13:09:55 +0100

    Solution of tests in .NET

---
+++ dotNet/ZeroMq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroMq
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var zmqReceiver = new ZMQReceiver();            
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ dotNet/ZeroMq/ZMQReceiver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroMq
+{
+    class ZMQReceiver
+    {
+
+        ZMQ.Context context = new ZMQ.Context();
+
+        public ZMQReceiver()
+        {
+            
+            var puller = context.Socket(ZMQ.SocketType.PULL);
+
+            var pool = puller.CreatePollItem(ZMQ.IOMultiPlex.POLLIN);
+
+            pool.PollInHandler += puller_PollInHandler;
+            pool.PollErrHandler +=  Program_PollErrHandler;
+
+            puller.Connect("tcp://192.168.1.75:12345");
+
+            while (true)
+            {
+
+                context.Poll(new ZMQ.PollItem[]{ pool }, 500);
+
+            }
+                        
+        }
+
+        void Program_PollErrHandler(ZMQ.Socket socket, ZMQ.IOMultiPlex revents)
+        {
+
+            Console.WriteLine("ERROR");
+
+        }
+
+        void puller_PollInHandler(ZMQ.Socket socket, ZMQ.IOMultiPlex revents)
+        {
+
+            var message = socket.Recv(UTF8Encoding.UTF8);
+
+            Console.WriteLine("Message received" + message);
+
+        }
+    }
+}
